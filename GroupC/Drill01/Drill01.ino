3501987688e6c6ebc6415e2f0f3c2c51675c4e07
Elaine-Yong <78578075+Elaine-Yong@users.noreply.github.com>
2021-02-08 17:55:49 +0800

    c LAI LE
    OKOK

--- GroupC/Drill01/Drill01.ino
+++ GroupC/Drill01/Drill01.ino
@@ -1,11 +1,29 @@
-void setup() 
+
+const int BUTTON1 = 2;
+const int BUTTON2 = 4;
+const int LED = 8;
+
+void setup()
 {
-  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(BUTTON1, INPUT);
+pinMode(BUTTON2, INPUT);
+pinMode(LED, OUTPUT);
 
 }
 
-void loop() 
+void loop()
 {
-  // put your main code here, to run repeatedly:
-
+  BUTTONstate1 = digitalRead(BUTTON1);
+  BUTTONstate2 = digitalRead(BUTTON2);
+  if (BUTTONstate1 == HIGH||BUTTONstate2 == HIGH)
+  {
+    digitalWrite(LED, HIGH);
+  }
+  else if (BUTTONstate1 == HIGH && BUTTONstate2 == HIGH) {
+    digitalWrite(LED, LOW);
+  }
+  else {
+    digitalWrite(LED,LOW);
+  }
 }
