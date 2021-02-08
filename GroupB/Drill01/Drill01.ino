9970bc6808a1ca91dff63723f1a6ff6e3d96a82f
BryanLeong1126 <bryanleong1126@gmail.com>
2021-02-08 17:50:15 +0800

    Update Drill01.ino

--- GroupB/Drill01/Drill01.ino
+++ GroupB/Drill01/Drill01.ino
@@ -1,11 +1,24 @@
-void setup() 
+void setup()
 {
-  // put your setup code here, to run once:
-
+  pinMode(13, INPUT);
+  pinMode(12, INPUT);
+  pinMode(11, OUTPUT);
 }
 
-void loop() 
+void loop()
 {
   // put your main code here, to run repeatedly:
-
+if (digitalRead(13)==0 && digitalRead(12)==0)
+ {
+  digitalWrite(11,0);
+}
+else if
+(digitalRead(13)==1 && digitalRead(12)==1
+{
+  digitalWrite(11,0);
+}
+else
+{
+  digitalWrite(11,1);
+}
 }
