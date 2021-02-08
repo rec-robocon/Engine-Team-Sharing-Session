c7e2f40c687cff7c03fc8b09043a16c913729128
CHIAWENHAN <wenhan1003@gmail.com>
2021-02-08 17:53:56 +0800

    Update Drill 1 Ans

--- GroupH/Drill01/Drill01.ino
+++ GroupH/Drill01/Drill01.ino
@@ -1,11 +1,25 @@
-void setup() 
-{
-  // put your setup code here, to run once:
+int button1 = 13;
+int button2 = 12;
+int led = 2
 
+void setup()
+{
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(led, OUTPUT);
 }
 
-void loop() 
+void loop()
 {
-  // put your main code here, to run repeatedly:
+  int b1 = digitalRead(button1);
+  int b2 = digitalRead(button2);
 
+  if ((b1 == 1 && b2 == 0) || (b1 == 0 && b2 == 1))
+  {
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+    digitalWrite(led, LOW);
+  }
 }
