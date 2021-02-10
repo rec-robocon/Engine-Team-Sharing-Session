a0e6b917f70775ed82b66dee4d06eb69f1e6ab9f
honjiajet <78597737+honjiajet@users.noreply.github.com>
2021-02-10 16:42:35 +0800

    Update Drill02.ino

--- GroupC/Drill02/Drill02.ino
+++ GroupC/Drill02/Drill02.ino
@@ -1,11 +1,45 @@
-void setup() 
-{
-  // put your setup code here, to run once:
+const int buttonpin=2, Led=3 ,d=7;
+int Pushbuttoncounter=0;
+bool buttonpress;
+string string(x);
 
+void setup()
+{
+  pinMode(buttonpin,INPUT);
+  pinMode(Led,OUTPUT);
 }
 
-void loop() 
+void loop()
 {
-  // put your main code here, to run repeatedly:
+  button= digitalRead(buttonpin);
+  if (button == LOW)
+  {
+    buttonpress = true;
+    Pushbuttoncounter++;
+  }
+  condition(Pushbuttoncounter);
+}
 
+void condition(int Pushbuttoncounter)
+{
+  if(Pushbuttoncounter%d==0)
+  {
+    digitalwrite(led,HIGH);
+    serialPrintln(Pushbuttoncounter);
+  }
+  else if (Pushbuttoncounter > 0)
+  {
+    while (Pushbuttoncounter > 0)
+    {
+      if (Pushbuttoncounter % 10 == d)
+      {
+        digitalWrite(Led, HIGH);
+        break;
+      }
+      Pushbuttoncounter = Pushbuttoncounter / 10;
+      else // doesnt meet both condution
+     {
+        digitalWrite(Led,LOW);
+     }
+  }
 }
