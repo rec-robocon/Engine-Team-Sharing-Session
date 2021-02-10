e159b3cf087cb938edcebfe6f32fa6dc9cd341ba
chiyyyyyang <78557677+chiyyyyyang@users.noreply.github.com>
2021-02-10 16:41:11 +0800

    Drill02

--- GroupG/Drill02/Drill02.ino
+++ GroupG/Drill02/Drill02.ino
@@ -1,11 +1,57 @@
-void setup() 
-{
-  // put your setup code here, to run once:
+int button = 10;
+int counter = 0;
+int led = 13;
+int ButtonPressed = 0;
 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(button, INPUT);
+  pinMode(led, OUTPUT);
 }
 
-void loop() 
+void loop()
 {
-  // put your main code here, to run repeatedly:
+  if(ButtonPressed == 0)
+  {
+    if(DigitalRead(button)==0)
+    {
+    }
+    else{
+      counter++;
+      ButtonPressed = 1;
+      delay(30);
+    }
+  }
+  else{
+    if(DigitalRead(button)==0)
+    {
+      ButtonPressed = 0;
+      delay(30);
+    }
+    else{
+
+    }
+  }
+
+  String x = String(counter);
+  for (int i=0; i<length(x); i++)
+  {
+    if (x[i]==7)
+    {
+      digitalWrite(led, HIGH);
+    }
+  }
+
+  if (counter%7 == 0)
+  {
+    digitalWrite(led, HIGH);
+  }
+
+  else
+  {
+    digitalWrite(led, LOW);
+  }
 
 }
+//Group G
