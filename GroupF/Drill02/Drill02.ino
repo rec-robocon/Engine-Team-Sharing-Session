d04a6aecc75bffb4cca7eaa35f252127b5487c6a
ChunWei01 <tanchunwei8681@gmail.com>
2021-02-10 16:34:15 +0800

    update

--- GroupF/Drill02/Drill02.ino
+++ GroupF/Drill02/Drill02.ino
@@ -1,11 +1,33 @@
-void setup() 
+const int buttonpin=4;
+const int ledpin=5;
+int total=0;
+int n=10;
+void setup()
 {
-  // put your setup code here, to run once:
+  pinmode(buttonpin,INPUT);
+  pinmode(ledpin,OUTPUT);
 
 }
 
-void loop() 
+void loop()
 {
-  // put your main code here, to run repeatedly:
+  if(digitalRead(buttonpin)==1)
+  {
+    if(digitalRead(buttonpin)==0)
+      total=total+1;
+  }
 
+  int temp = total;//version 1
+  while(temp != 0 ){
+    int remainder = temp%10;
+    if(remainder==7){
+      digitalWrite(ledpin, HIGH);
+    }
+    temp /= 10;
+  }
+
+  if(total % 7 == 0 || ((total-n)==7)){//version 2,
+    digitalWrite(ledpin,HIGH);
+    n=n+10;
+    }
 }
