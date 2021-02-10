f8115e46e5a8bbcf5024ce85fc1a4d2300d07770
Shi hong <shtan0730@gmail.com>
2021-02-10 16:32:14 +0800

    Update Drill02.ino

--- GroupE/Drill02/Drill02.ino
+++ GroupE/Drill02/Drill02.ino
@@ -3,7 +3,7 @@ int led=12;
 int counter=0;
 
 void setup()
-{ Serial.begin(9600);
+{ serial.begin(9600);
   pinMode(btn,INPUT);
   pinMode(led,OUTPUT);
 
@@ -14,7 +14,7 @@ void ledlight(int n)
 {
   if(n%7==0||n%10==7)
   {
-    digitalWirte(led,1);
+    digitalWrite(led,1);
   }
   else{
     digitalWrite(led,0);
