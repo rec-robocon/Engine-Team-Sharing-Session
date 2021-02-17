9883b3aa6b8c865559504229856ab7fc92e18506
Amirul Imran bin Roslan <48555955+Amirul-Imran@users.noreply.github.com>
2021-02-17 18:26:34 +0800

    Updated Drill05.ino
    Group A

--- GroupA/Drill05/Drill05.ino
+++ GroupA/Drill05/Drill05.ino
@@ -1,22 +1,16 @@
+int x;
+
 void setup()
 {
-pinMode(4, OUTPUT);
-pinMode(2, INPUT);
-pinMode(3, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-if (digitalRead(2) == 1)
+  while(Serial.available())
   {
-    if (digitalRead(3) == 0)
-      digitalWrite(4) = 1;
-    else
-      digitalWrite(4) =0;
-  }
-else
-  {
-    if (digitalRead(3) ==1)
-      digitalWrite(4) =1;
+    x += Serial.read();
+    Serial.print("The total amount is ");
+    Serial.println(x);
   }
 }
